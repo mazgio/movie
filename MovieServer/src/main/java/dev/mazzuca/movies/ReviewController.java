537d1eaa1c860e9affe86c2e946cf16424b106c6
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2023-08-05 19:01:30 +0200

    added cors in review controller

--- MovieServer/src/main/java/dev/mazzuca/movies/ReviewController.java
+++ MovieServer/src/main/java/dev/mazzuca/movies/ReviewController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
