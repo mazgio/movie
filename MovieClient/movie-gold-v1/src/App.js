5bcc253f8ae21564ea30e965158c2e34b5cf4107
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2023-09-19 10:39:59 +0200

    movie is undefined

--- MovieClient/movie-gold-v1/src/App.js
+++ MovieClient/movie-gold-v1/src/App.js
@@ -52,7 +52,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home movies={movies} />}></Route>
           <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
-          <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews} />}></Route>
+          <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} movie={console.log(movie)} reviews={reviews} setReviews={setReviews} />}></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Route>
       </Routes>
